da58ff917a57d0efe36a57009145f197a3375ee7
Renato Alves Soares <renatoguara2020@gmail.com>
2022-10-29 11:42:41 -0300

    ok

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ $db = new Database;
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,6 +15,7 @@ $db = new Database;
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?=URL?>/public/css/estilos.css">
 </head>
+
 <body>
     <?php
         include '../app/Views/topo.php';
@@ -22,7 +24,8 @@ $db = new Database;
     ?>
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script> 
-    <script src="<?=URL?>/public/js/jquery.funcoes.js"></script> 
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <script src="<?=URL?>/public/js/jquery.funcoes.js"></script>
 </body>
+
 </html>
